84eeddc871215e127ebe902cd3a0dbcea3b916f3
Your Name <ruslankurman13@gmail.com>
2024-07-30 14:37:59 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+"use strict";
+document.addEventListener("click", function(e) {
+    const spider = document.querySelector(".spider");
+    const wall = document.querySelector(".wall");
+    const wallY = wall.clientHeight;
+    const wallX = wall.clientWidth;
+    const spiderY = spider.clientHeight;
+    const spiderX = spider.clientWidth;
+    const clickY = e.offsetY;
+    const clickX = e.offsetX;
+    const maxX = wallX - spiderX;
+    const maxY = wallY - spiderY;
+    const goToX = Math.max(0, Math.min(clickX - spiderX / 2, maxX));
+    const goToY = Math.max(0, Math.min(clickY - spiderY / 2, maxY));
+    spider.style.top = `${goToY}px`;
+    spider.style.left = `${goToX}px`;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
---
+++ index.ffc7b5ad.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(t){var e=document.querySelector(".spider"),c=document.querySelector(".wall"),n=c.clientHeight,i=c.clientWidth,l=e.clientHeight,a=e.clientWidth,o=t.offsetY,d=t.offsetX;e.style.top="".concat(Math.max(0,Math.min(o-l/2,n-l)),"px"),e.style.left="".concat(Math.max(0,Math.min(d-a/2,i-a)),"px")});
+//# sourceMappingURL=index.ffc7b5ad.js.map
